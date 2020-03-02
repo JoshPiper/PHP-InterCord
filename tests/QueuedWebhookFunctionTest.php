cc769ad1bcecd35d7a37655986dc1bf4ffaf6b4e
John Internet <jonjon1234.github@gmail.com>
2020-03-02 22:31:39 +0000

    This no longer accepts arguments.

--- tests/QueuedWebhookFunctionTest.php
+++ tests/QueuedWebhookFunctionTest.php
@@ -18,7 +18,7 @@ class QueuedWebhookFunctionTest extends TestCase {
 		unset($this->webhook);
 	}
 
-	public function testExecution($content, $username, $avatar){
+	public function testExecution(){
 		$contents = ['content', '1234', 4321, ['array with one arg', (new \Internet\InterCord\RichEmbed())->addField("hi", "yes")]];
 		$usernames = ['', 'testUser'];
 		$avatars = ['', 'https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip'];

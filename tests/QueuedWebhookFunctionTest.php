9be4eb4c942a4c29f5544f68868f9808f1af453c
John Internet <jonjon1234.github@gmail.com>
2020-03-02 22:14:41 +0000

    Remove await, make the contents array smaller.

--- tests/QueuedWebhookFunctionTest.php
+++ tests/QueuedWebhookFunctionTest.php
@@ -17,17 +17,14 @@ class QueuedWebhookFunctionTest extends TestCase {
 	}
 
 	public function executionProvider(){
-		$contents = ['content', 'content 2', '1234', 4321, ['array with one arg'], ['array with', 'two args'], ['array with', 1234, 'types']];
+		$contents = ['content', '1234', 4321, ['array with one arg', (new \Internet\InterCord\RichEmbed())->addField("hi", "yes")]];
 		$usernames = ['', 'testUser', 1234];
 		$avatars = ['', 'https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip'];
-		$awaits = [true, false];
 
 		foreach ($avatars as $avatar){
 			foreach ($usernames as $username){
 				foreach ($contents as $content){
-					foreach ($awaits as $await){
-						yield [$content, $username, $avatar, $await];
-					}
+					yield [$content, $username, $avatar];
 				}
 			}
 		}
@@ -36,8 +33,8 @@ class QueuedWebhookFunctionTest extends TestCase {
 	/**
 	 * @dataProvider executionProvider
 	 */
-	public function testExecution($content, $username, $avatar, $await){
-		$this->webhook->append($content, $username, $avatar, $await);
+	public function testExecution($content, $username, $avatar){
+		$this->webhook->append($content, $username, $avatar);
 		$this->webhook->run(1);
 
 		$this->addToAssertionCount(1);

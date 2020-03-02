500b2690eead66be141ee8952b8d811823f7e65a
John Internet <jonjon1234.github@gmail.com>
2020-03-02 22:15:29 +0000

    We don't need a numeric username.

--- tests/QueuedWebhookFunctionTest.php
+++ tests/QueuedWebhookFunctionTest.php
@@ -18,7 +18,7 @@ class QueuedWebhookFunctionTest extends TestCase {
 
 	public function executionProvider(){
 		$contents = ['content', '1234', 4321, ['array with one arg', (new \Internet\InterCord\RichEmbed())->addField("hi", "yes")]];
-		$usernames = ['', 'testUser', 1234];
+		$usernames = ['', 'testUser'];
 		$avatars = ['', 'https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip'];
 
 		foreach ($avatars as $avatar){

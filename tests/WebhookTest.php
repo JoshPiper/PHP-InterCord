ea50c9f4f2602a50e5ccf37dcb47d2ef45da3f17
John Internet <jonjon1234.github@gmail.com>
2020-03-04 23:04:23 +0000

    Merged all the stuff together and added an empty send to error.

--- tests/WebhookTest.php
+++ tests/WebhookTest.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 use Internet\InterCord\Webhook;
+use GuzzleHttp\Exception\RequestException;
 
-final class WebhookSetupTest extends TestCase {
+class WebhookTest extends TestCase {
+	protected $webhook;
+	protected $url;
+	protected $id;
+	protected $token;
 	protected function setUp(): void{
 		parent::setUp();
+		$this->webhook = new Webhook($_SERVER['WEBHOOK_URL']);
 		['WEBHOOK_URL' => $this->url, 'WEBHOOK_ID' => $this->id, 'WEBHOOK_TOKEN' => $this->token] = $_SERVER;
 	}
 
@@ -24,5 +30,10 @@ final class WebhookSetupTest extends TestCase {
 	public function testCreationFromReverseTokenPair(): void {
 		$this->expectException(InvalidArgumentException::class);
 		$webhook = new Webhook($this->token, $this->id);
+	}
+
+	public function testEmptyException(): void {
+		$this->expectException(RequestException::class);
+		$this->webhook->execute('');
 	}
 }

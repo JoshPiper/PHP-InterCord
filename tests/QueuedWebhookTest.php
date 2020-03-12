a28e8e12c7462ff5eeb437a6aa4aa24736739001
John Internet <jonjon1234.github@gmail.com>
2020-03-12 17:44:25 +0000

    Move data to a dataProvider. Add test for delivery. Changed assertion to check that the webhook is empty.

--- tests/QueuedWebhookTest.php
+++ tests/QueuedWebhookTest.php
@@ -17,21 +17,43 @@ class QueuedWebhookTest extends TestCase {
 		unset($this->webhook);
 	}
 
-	public function testExecution(){
+	public function payloads(){
 		$contents = ['content', '1234', 4321, ['array with one arg', (new \Internet\InterCord\RichEmbed())->addField("hi", "yes")]];
 		$usernames = ['', 'testUser'];
 		$avatars = ['', 'https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip'];
 
-		foreach ($avatars as $avatar){
+		foreach ($contents as $content){
 			foreach ($usernames as $username){
-				foreach ($contents as $content){
-					$this->webhook->append($content, $username, $avatar);
-					$this->addToAssertionCount(1);
+				foreach ($avatars as $avatar){
+					yield [$content, $username, $avatar];
 				}
 			}
 		}
+	}
+
+	/**
+	 * @dataProvider payloads
+	 * @param $content
+	 * @param $username
+	 * @param $avatar
+	 * @throws Exception
+	 */
+	public function testExecution($content, $username, $avatar){
+		$this->webhook->append($content, $username, $avatar);
 		$this->webhook->run();
-		$this->addToAssertionCount(1);
+		$this->assertEquals($this->webhook->count(), 0, "empty");
+	}
+
+	/**
+	 * @dataProvider payloads
+	 * @param $content
+	 * @param $username
+	 * @param $avatar
+	 * @throws Exception
+	 */
+	public function testDelivery($content, $username, $avatar){
+		$this->webhook->deliver($content, $username, $avatar);
+		$this->assertEquals($this->webhook->count(), 0, "empty");
 	}
 
 	public function testEmptyMessageException(){
